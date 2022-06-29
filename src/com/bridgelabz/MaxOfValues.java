0f3cafcc7a59c591dfbb20a482fa6caab60f04d9
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-29 20:54:28 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/MaxOfValues.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MaxOfValues {
+    //main method starts
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generics Programs");
+    }
+}
