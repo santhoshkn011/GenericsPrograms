21573bc468bac43a1ef2213041b11eb50722d15b
Santhosh_Kumar_Nayak <santhoshkn011@gmail.com>
2022-06-29 23:00:13 +0530

    Modified: Calculating max using sorting

--- src/com/bridgelabz/MaxOfValues.java
+++ src/com/bridgelabz/MaxOfValues.java
@@ -1,34 +1,48 @@
 /*
-Refactor to create Generic Class to take in 3 variables of Generic Type
-compareTo method compares two strings
- if s1 > s2, it returns positive number
-if s1 < s2, it returns negative number
-if s1 == s2, it returns 0
+Extend the max method to take more than three parameters
+-Use Options and Sorting
  */
 package com.bridgelabz;
+
+import java.util.Arrays;
+
 //Generic Class
 public class MaxOfValues <E extends Comparable<E>>{
-    E a1 , a2, a3; //Generics class variables
-    //constructor
-    public MaxOfValues(E a1, E a2, E a3) {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
+    private E[] eArray; //declaring array
+    //Constructor
+    MaxOfValues(E[] eArray) {
+        this.eArray = eArray;
+    }
+    public void display() {
+        System.out.println("\nBefore Sorting:");
+        for(E element : eArray) {
+            System.out.printf("%s", element + ", ");
+        }
+        Arrays.sort(eArray);
+        System.out.println("\nAfter Sorting:");
+        for(E element : eArray) {
+            System.out.printf("%s", element + ", ");
+        }
     }
     //Generics method to find max value
-    public static <E extends Comparable<E>> E findMaxValue(E a1, E a2, E a3) {
-        E max = a1; //initially a1 is largest
-        if (a2.compareTo(max) > 0)
-            max = a2;
-        if (a3.compareTo(max) > 0)
-            max = a3;
-        return max;
+    public void findMaxValue() {
+        int lengthOfArray = eArray.length; //local variable
+        Arrays.sort(eArray); //sorting the array
+        System.out.println(eArray[lengthOfArray -1]); //after sorting the last element will be the max
     }
     //main method starts
     public static void main(String[] args) {
+        //Declaring the values to the arrays
+        Integer[] integerValue = {110, 750, 150, 365, 450, 55};
+        Float[] floatValue = {44.98f, 18.70f, 99.06f, 88.35f, 26.85f};
+        String[] stringValue = {"Santhosh", "Kumar", "Nayak", "Namitha"};
+        //Displaying the elements before and after sorting
+        new MaxOfValues(integerValue).display(); System.out.println();
+        new MaxOfValues(floatValue).display(); System.out.println();
+        new MaxOfValues(stringValue).display(); System.out.println();
         //printing the max values
-        System.out.println("The maximum of three integers: " +findMaxValue(110,200,150));
-        System.out.println("The maximum of three floats: " +findMaxValue(44.98f, 18.70f, 99.06f));
-        System.out.println("The maximum of three strings: " +findMaxValue("Australia", "Japan", "Canada"));
+        System.out.print("\nThe maximum of integers: " ); new MaxOfValues(integerValue).findMaxValue();
+        System.out.print("\nThe maximum of floats: "); new MaxOfValues(floatValue).findMaxValue();
+        System.out.print("\nThe maximum of strings: " ); new MaxOfValues(stringValue).findMaxValue();
     }
 }
